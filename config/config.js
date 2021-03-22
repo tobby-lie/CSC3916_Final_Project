cb2d6856dfbdb4fb3ec4f861964b673a6dbd1a07
Tobby Lie <tobbylie@gmail.com>
2021-03-22 15:52:34 -0600

    initial commit

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ const config = {
   mongoUri: process.env.MONGODB_URI ||
     process.env.MONGO_HOST ||
       'mongodb+srv://admin:admin123@cluster0.0mkjp.mongodb.net/mernproject?retryWrites=true&w=majority',
-  stripe_connect_test_client_id: 'ca_JADPmwNo4KNFsWibJGAlRPgrxmuaehFi',
+  stripe_connect_test_client_id: 'ca_JADPD4HoVcucYXzVg3ecepHKyiaC85o1',
   stripe_test_secret_key: 'sk_test_51IXsL8EXPIkGMjJvlTGFpJzRox8Wau9lmdJmQgxzR0KNiY57XmNYOssHKr1Z1M5CsW07oRr5auw9MQTaVtFeOfAS00aJgm4k5y',
   stripe_test_api_key: 'pk_test_51IXsL8EXPIkGMjJvpCqoLGYVpQQeZXBw50BYZMjUBRX1n7HgwrAolU9P4oBlcz29LThXENz1rcMsmoxhw0jhAFQs003lXLFuwf'
 }
