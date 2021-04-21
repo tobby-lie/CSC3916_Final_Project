30c3d450a0c8740479be22dae415e7d16fdff6a7
Ryan Ellis <ryan.ellis33@gmail.com>
2021-04-21 13:53:39 -0600

    Able to diplay Charities, work on NewCharity

--- client/charity/NewCharity.js
+++ client/charity/NewCharity.js
@@ -1,9 +1,120 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Card from '@material-ui/core/Card'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import Button from '@material-ui/core/Button'
+import FileUpload from '@material-ui/icons/AddPhotoAlternate'
+import auth from './../auth/auth-helper'
+import TextField from '@material-ui/core/TextField'
+import Typography from '@material-ui/core/Typography'
+import Icon from '@material-ui/core/Icon'
+import { makeStyles } from '@material-ui/core/styles'
+import {create} from './api-charity.js'
+import {Link, Redirect} from 'react-router-dom'
+
+const useStyles = makeStyles(theme => ({
+    card: {
+      maxWidth: 600,
+      margin: 'auto',
+      textAlign: 'center',
+      marginTop: theme.spacing(5),
+      paddingBottom: theme.spacing(2)
+    },
+    error: {
+      verticalAlign: 'middle'
+    },
+    title: {
+      marginTop: theme.spacing(2),
+      color: theme.palette.openTitle,
+      fontSize: '1em'
+    },
+    textField: {
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+      width: 300
+    },
+    submit: {
+      margin: 'auto',
+      marginBottom: theme.spacing(2)
+    },
+    input: {
+      display: 'none'
+    },
+    filename:{
+      marginLeft:'10px'
+    }
+  }))
 
 export default function NewCharity() {
+    const classes = useStyles()
+       
+    const [values, setValues] = useState({
+        name: '',
+        description: ''
+        
+  })
+
+  const jwt = auth.isAuthenticated()
+
+  const handleChange = name => event => {
+    const value = name === 'image'
+      ? event.target.files[0]
+      : event.target.value
+    setValues({...values, [name]: value })
+  }
+
+
+  const clickSubmit = () => {
+    let charityData = new FormData()
+    values.name && charityData.append('name', values.name)
+    values.description && charityData.append('description', values.description)
+    create({
+        userId: jwt.user._id
+    }, {
+        t: jwt.token
+    }, charityData).then((data) => {
+      if (data.error) {
+        setValues({...values, error: data.error})
+      } else {
+        setValues({...values, error: '', redirect: true})
+      }
+    })
+  }
+    if (values.redirect) {
+        return (<Redirect to={'/seller/shops'}/>)
+    }
+       
     return (
         <div>
-            Hello New Charity
+            <Card className={classes.card}>
+                <CardContent>
+                    <Typography type="headline" component="h2" className={classes.title}>
+                        New Charity
+                    </Typography>
+                    <br/>
+                    
+                    <TextField id="name" label="Name" className={classes.textField} value={values.name} onChange={handleChange('name')} margin="normal"/><br/>
+                    <TextField
+                        id="multiline-flexible"
+                        label="Description"
+                        multiline
+                        rows="2"
+                        value={values.description}
+                        onChange={handleChange('description')}
+                        className={classes.textField}
+                        margin="normal"
+                    /><br/> {
+                        values.error && (<Typography component="p" color="error">
+                            <Icon color="error" className={classes.error}>error</Icon>
+                        {values.error}</Typography>)
+                    }
+                </CardContent>
+                <CardActions>
+                    <Button color="primary" variant="contained" onClick={clickSubmit} className={classes.submit}>Submit</Button>
+                    <Link to='/charity/shop/new' className={classes.submit}><Button variant="contained">Cancel</Button></Link>
+                </CardActions>
+            </Card>
         </div>
     )
 }
+
---
+++ client/charity/api-charity.js
@@ -0,0 +1,84 @@
+const create = async (params, credentials, shop) => {
+    try {
+      let response = await fetch('/api/charities/by/'+ params.userId, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Authorization': 'Bearer ' + credentials.t
+        },
+        body: shop
+      })
+        return response.json()
+      } catch(err) { 
+        console.log(err)
+      }
+  }
+
+  const list = async (signal) => {
+    try {
+      let response = await fetch('/api/charities', {
+        method: 'GET',
+        signal: signal
+      })
+      return response.json()
+    }catch(err) {
+      console.log(err)
+    }
+  }
+
+  const listByOwner = async (params, credentials, signal) => {
+    try {
+      let response = await fetch('/api/charities/by/'+params.userId, {
+        method: 'GET',
+        signal: signal,
+        headers: {
+          'Accept': 'application/json',
+          'Authorization': 'Bearer ' + credentials.t
+        }
+      })
+      return response.json()
+    }catch(err){
+      console.log(err)
+    }
+  }
+
+  const update = async (params, credentials, shop) => {
+    try {
+      let response = await fetch('/api/charities/' + params.charityId, {
+        method: 'PUT',
+        headers: {
+          'Accept': 'application/json',
+          'Authorization': 'Bearer ' + credentials.t
+        },
+        body: shop
+      })
+      return response.json()
+    } catch(err) {
+      console.log(err)
+    }
+  }
+
+  const remove = async (params, credentials) => {
+    try {
+      let response = await fetch('/api/charities/' + params.charityId, {
+        method: 'DELETE',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + credentials.t
+        }
+      })
+      return response.json()
+    } catch(err) {
+      console.log(err)
+    }
+  }
+
+  export {
+    create,
+    list,
+    listByOwner,
+    //read,
+    update,
+    remove
+  }
