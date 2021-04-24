5f5ef6a4f303385fe1ca03e84935a88d90168f94
Ryan Ellis <ryan.ellis33@gmail.com>
2021-04-24 17:39:36 -0600

    added api-donations

---
+++ client/charity/api-donations.js
@@ -0,0 +1,32 @@
+const listByCharity = async (params, credentials, signal) => {
+    try {
+      let response = await fetch('/api/donations/from/' + params.charityId, {
+        method: 'GET',
+        signal: signal,
+        headers: {
+          'Accept': 'application/json',
+          'Authorization': 'Bearer ' + credentials.t
+        }
+      })
+      return response.json()
+    }catch(err){
+      console.log(err)
+    }
+  }
+ 
+  const listByOwner = async (params, credentials, signal) => {
+    try {
+      let response = await fetch('/api/donations/by/' +params.userId, {
+        method: 'GET',
+        signal: signal,
+        headers: {
+          'Accept': 'application/json',
+          'Authorization': 'Bearer ' + credentials.t
+        }
+      })
+      return response.json()
+    }catch(err){
+      console.log(err)
+    }
+  }
+  export { listByCharity, listByOwner }
